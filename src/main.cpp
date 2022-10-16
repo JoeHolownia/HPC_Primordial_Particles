3b23bfc07d30a753e01d221cfc11d62f949b91b4
JoeHolownia <joeholownia7@gmail.com>
2022-10-16 17:12:30 +1000

    Started MPI implementation... is a complete mess right now!

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,29 @@
 #include "universe.h"
 #include "ioparser.h"
 #include "nlohmann/json.hpp"
+#include <mpi.h>
 using json = nlohmann::json;
+
+void CalculateGridLayout(int nProcs, int *gridRows, int *gridCols); // PLEASE MOVE THIS TO A SEPERATE HEADER FILE, MAYBE MPI HELPERS??
+
+struct cord {
+    float x0;
+    float x1;
+    float y0;
+    float y1;
+} ;
+
+// struct part_cord {
+//     float x;
+//     float y;
+//     float vx;
+//     float vy;
+// } ;
+
+typedef struct cord cord_t ;
+// typedef struct part_cord pcord_t ;
+
+// MOVE THE ABOVE INTO AN APPROPRIATE HEADER FILE!!!! DON'T MAKE THIS CODE COMPLETELY SHIT PLEASE!!!
 
 void SystemCommandCall(std::string command) {
     /**
@@ -55,36 +77,108 @@ struct Timer {
   }
 };
 
+void calculate_grid_layout(int nProcs, int *gridRows, int *gridCols)
+{
+    int i;
+    for (i = sqrt(nProcs); i > 0; i--)
+    {
+        if (nProcs % i == 0)
+        {
+            *gridRows = i;
+            *gridCols = nProcs / i;
+            return;
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
 
-    // instantiate io parser
-    IOParser io_parser("settings.json", "out_display.bin", "out_log.bin");
+    // initalise MPI
+    int rank, num_procs;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    // read in json data
-    std::ifstream json_settings_file("settings.json");
-    json settings = json::parse(json_settings_file);
-    json_settings_file.close();
-    int num_particles = settings["num_particles"].get<int>();
-    int width = settings["width"].get<int>();
-    int height = settings["height"].get<int>();
-    float density = settings["density"].get<float>();
-    float alpha = settings["alpha"].get<float>();
-    float beta = settings["beta"].get<float>();
-    float gamma = settings["gamma"].get<float>();
-    int time_steps = settings["time_steps"].get<int>();
+	// create MPI custom type in order to ease the communication. // TODO: MODIFY THIS TO WORK FOR OUR PARTICLES
+    MPI_Datatype mpi_particle_type, oldTypes[2] = {MPI_FLOAT, MPI_INT};
+    int blockcounts[2] = {4, 1};
+    MPI_Aint offsets[2], extent;
+    offsets[0] = 0;
+    MPI_Type_extent(MPI_FLOAT, &extent);
+    offsets[1] = 4 * extent;
+    MPI_Type_struct(2, blockcounts, offsets, oldTypes, &mpi_particle_type);
+    MPI_Type_commit(&mpi_particle_type);
 
+	// global universe variables
+	int global_num_particles, global_width, global_height, time_steps;
+	float density, alpha, beta, gamma;
+
+	// instantiate io parser
+	IOParser io_parser;
+    if (rank == 0) {
+		io_parser("settings.json", "out_display.bin", "out_log.bin");
+
+		// read in json data: TODO: THIS IS PROBLEMATIC ATM, EITHER NEED TO STORE THESE GLOBALLY OR READ IN AND THEN DISTRIBUTE TO EACH PROCESS
+		std::ifstream json_settings_file("settings.json");
+		json settings = json::parse(json_settings_file);
+		json_settings_file.close();
+		global_num_particles = settings["num_particles"].get<int>();
+		global_width = settings["width"].get<int>();
+		global_height = settings["height"].get<int>();
+		density = settings["density"].get<float>();
+		alpha = settings["alpha"].get<float>();
+		beta = settings["beta"].get<float>();
+		gamma = settings["gamma"].get<float>();
+		time_steps = settings["time_steps"].get<int>();
+    }
+
+    // init MPI grid communicator
+    MPI_Comm gridComm;
+    MPI_Request request[4];
+    MPI_Status status[4];  // I BELIEVE 4 IS BECAUSE OF THE left, top, right, down --> if we do diagonal this needs to be 8
+
+	// init grid variables
+    int grid_rows, grid_cols, reorder = 1, neighbours[4], send_counts[4]={0}, dims[2], my_coords[2], periods[2] = {1, 1};
+
+	// create a division of processes in 2-D cartesian grid.
+	calculate_grid_layout(num_procs, &grid_rows, &grid_cols);
+    dims[0] = grid_rows;
+    dims[1] = grid_cols;
+    MPI_Dims_create(num_procs, 2, dims); 
+
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &gridComm);
+    MPI_Cart_get(gridComm, 2, dims, periods, my_coords); // myCoords[0] = row, myCoords[0] = col.
+
+    //calculate the local box dimension.
+	cord_t local_box;
+    int local_box_width = global_width / grid_cols;
+    int local_box_height = global_height / grid_rows;
+    local_box.x0 = myCoords[1] * local_box_width;
+    local_box.y0 = myCoords[0] * local_box_height;
+    local_box.x1 = (myCoords[1] == gridCols-1) ? gBoxWidth : (myCoords[1]+1) * lBoxWidth;
+    local_box.y1 = (myCoords[0] == gridRows-1) ? gBoxHeight : (myCoords[0]+1) * lBoxHeight;
+
+	// TODO: HERE WE ARE!!!!! ==============================^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    //Get the rank of neighbor processors.
+    MPI_Cart_shift(gridComm, 0, 1, &neighbours[DOWN], &neighbours[UP]);
+    MPI_Cart_shift(gridComm, 1, 1, &neighbours[LEFT], &neighbours[RIGHT]);
+
+	// THIS NEED STO BE CHANGED TO THE INSTANTIATION OF EACH MINIVERSE
     // instantiate universe
     Universe universe(num_particles, width, height, density, alpha, beta, gamma);
 
     // instante IO (i.e. class which handles keeping log file stream open, formatting for reading/writing)
-    io_parser.OpenOutFile();
-    io_parser.WriteStateToOutFile(universe.GetCurrentState(), universe.GetNumParticles());
+    if (rank == 0) {
+		io_parser.OpenOutFile();
+		io_parser.WriteStateToOutFile(universe.GetCurrentState(), universe.GetNumParticles());
+    }
 
     // run simulation for all time steps
     for (int i = 0; i < time_steps; i++) {
-      // Timer text_time;
-      universe.Step();
-      io_parser.WriteStateToOutFile(universe.GetCurrentState(), universe.GetNumParticles());
+		// Timer text_time;
+		universe.Step();
+		io_parser.WriteStateToOutFile(universe.GetCurrentState(), universe.GetNumParticles());
     }
 
     // output average time
@@ -95,9 +189,11 @@ int main(int argc, char *argv[]) {
     io_parser.CloseOutFile();
     universe.Clean();
 
-    // call Python to plot data 
-     std::string command = "python3 display.py";
-     SystemCommandCall(command);
+    if (rank == 0) {
+    	// call Python to plot data 
+		std::string command = "python3 display.py";
+		SystemCommandCall(command);
+    }
 
     return 0;
 }
